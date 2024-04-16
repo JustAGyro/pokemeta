9ffa29c663335f633f703d4dcbadacc5a87a4948
gyro <kamran.zq@gmail.com>
2024-04-16 00:33:22 -0500

    testing git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'PokeMeta',
-  description: 'Show off your PokePower!',
+  description: 'Show off your PokePower! Rawr',
 };
 
 export default function RootLayout({
